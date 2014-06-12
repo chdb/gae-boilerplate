f517e6fd1156e3ff696d619da082e76dea813d8d
chdb <mr.colin.barnett@gmail.com>
2014-06-12 17:25:48 +0100

    formatting etc

--- bp_content/themes/default/handlers/handlers.py
+++ bp_content/themes/default/handlers/handlers.py
@@ -48,50 +48,39 @@ class ContactHandler(BaseHandler):
 
     def post(self):
         """ validate contact form """
-
         if not self.form.validate():
             return self.get()
-        remote_ip = self.request.remote_addr
-        city = i18n.get_city_code(self.request)
-        region = i18n.get_region_code(self.request)
-        country = i18n.get_country_code(self.request)
-        coordinates = i18n.get_city_lat_long(self.request)
+            
         user_agent = self.request.user_agent
         exception = self.request.POST.get('exception')
-        name = self.form.name.data.strip()
-        email = self.form.email.data.lower()
         message = self.form.message.data.strip()
         template_val = {}
 
         try:
-            # parsing user_agent and getting which os key to use
-            # windows uses 'os' while other os use 'flavor'
+            # parse user_agent to get operating system 
             ua = httpagentparser.detect(user_agent)
-            _os = ua.has_key('flavor') and 'flavor' or 'os'
-
-            operating_system = str(ua[_os]['name']) if "name" in ua[_os] else "-"
-            if 'version' in ua[_os]:
-                operating_system += ' ' + str(ua[_os]['version'])
+            osKey = 'flavor'            if ua.has_key('flavor') else 'os'  # windows uses 'os' while others use 'flavor'
+            os = str(ua[osKey]['name']) if "name" in ua[osKey]  else "-"
+            if 'version' in ua[osKey]:
+                os += ' ' + str(ua[osKey]['version'])
             if 'dist' in ua:
-                operating_system += ' ' + str(ua['dist'])
+                os += ' ' + str(ua['dist'])
 
-            browser = str(ua['browser']['name']) if 'browser' in ua else "-"
-            browser_version = str(ua['browser']['version']) if 'browser' in ua else "-"
+            browser  = str(ua['browser']['name'])    if 'browser' in ua else "-"
+            bversion = str(ua['browser']['version']) if 'browser' in ua else "-"
 
-            template_val = {
-                "name": name,
-                "email": email,
-                "ip": remote_ip,
-                "city": city,
-                "region": region,
-                "country": country,
-                "coordinates": coordinates,
-
-                "browser": browser,
-                "browser_version": browser_version,
-                "operating_system": operating_system,
-                "message": message
-            }
+            template_val =  { "name"          : self.form.name.data.strip()
+                            , "email"         : self.form.email.data.lower()
+                            , "ip"            : self.request.remote_addr
+                            , "city"          : i18n.get_city_code(self.request)
+                            , "region"        : i18n.get_region_code(self.request)
+                            , "country"       : i18n.get_country_code(self.request)
+                            , "coordinates"   : i18n.get_city_lat_long(self.request)
+                            , "browser"       : browser
+                            , "browser_version": bversion
+                            , "operating_system": os
+                            , "message"       : self.form.message.data.strip()
+                            }
         except Exception as e:
             logging.error("error getting user agent info: %s" % e)
 
@@ -106,18 +95,17 @@ class ContactHandler(BaseHandler):
 
             email_url = self.uri_for('taskqueue-send-email')
             taskqueue.add(url=email_url, params={
-                'to': self.app.config.get('contact_recipient'),
-                'subject': subject,
-                'body': body,
-                'sender': self.app.config.get('contact_sender'),
-            })
+                'to': self.app.config.get('contact_recipient')
+                , 'subject': subject
+                , 'body': body
+                , 'sender': self.app.config.get('contact_sender')
+            , })
 
             self.flash('success', _('Your message was sent successfully.'))
             return self.redirect_to('contact')
 
         except (AttributeError, KeyError), e:
-            logging.error('Error sending contact form: %s' % e)
-            
+            logging.error('Error sending contact form: %s' % e)       
             self.flash('error', _('Error sending the message. Please try again later.'))
             return self.redirect_to('contact')
 
@@ -141,14 +129,14 @@ class SecureRequestHandler(BaseHandler):
             user_session['user_id'], user_session['token'])
 
         try:
-            params = {
-                "user_session": user_session,
-                "user_session_object": user_session_object,
-                "user_info": user_info,
-                "user_info_object": user_info_object,
-                "userinfo_logout-url": self.auth_config['logout_url'],
-            }
+            params ={ "user_session": user_session
+                    , "user_session_object": user_session_object
+                    , "user_info": user_info
+                    , "user_info_object": user_info_object
+                    , "userinfo_logout-url": self.auth_config['logout_url']
+                    }
             return self.render_template('secure_zone.html', **params)
+        
         except (AttributeError, KeyError), e:
             return "Secure zone error:" + " %s." % e
 
@@ -157,32 +145,32 @@ class DeleteAccountHandler(BaseHandler):
 
     @user_required
     def get(self, **kwargs):
-        chtml = captcha.displayhtml(
-            public_key=self.app.config.get('captcha_public_key'),
-            use_ssl=(self.request.scheme == 'https'),
-            error=None)
-        if self.app.config.get('captcha_public_key') == "PUT_YOUR_RECAPCHA_PUBLIC_KEY_HERE" or \
-                        self.app.config.get('captcha_private_key') == "PUT_YOUR_RECAPCHA_PUBLIC_KEY_HERE":
-            chtml = '<div class="alert alert-error"><strong>Error</strong>: You have to ' \
-                    '<a href="http://www.google.com/recaptcha/whyrecaptcha" target="_blank">sign up ' \
-                    'for API keys</a> in order to use reCAPTCHA.</div>' \
+        chtml = captcha.displayhtml ( public_key=self.app.config.get('captcha_public_key')
+                                    , use_ssl=(self.request.scheme == 'https')
+                                    , error=None)
+        if self.app.config.get('captcha_public_key')  == "PUT_YOUR_RECAPCHA_PUBLIC_KEY_HERE" \
+        or self.app.config.get('captcha_private_key') == "PUT_YOUR_RECAPCHA_PUBLIC_KEY_HERE":
+            chtml = '<div class="alert alert-error"><strong>Error</strong>' \
+                        ': You have to ' \
+                            '<a href="http://www.google.com/recaptcha/whyrecaptcha" target="_blank">' \
+                                'sign up for API keys' \
+                            '</a>' \
+                        ' in order to use reCAPTCHA.' \
+                    '</div>' \
                     '<input type="hidden" name="recaptcha_challenge_field" value="manual_challenge" />' \
-                    '<input type="hidden" name="recaptcha_response_field" value="manual_challenge" />'
-        params = {
-            'captchahtml': chtml,
-        }
+                    '<input type="hidden" name="recaptcha_response_field"  value="manual_challenge" />'
+        
+        params = { 'captchahtml': chtml }
         return self.render_template('delete_account.html', **params)
 
     def post(self, **kwargs):
         challenge = self.request.POST.get('recaptcha_challenge_field')
-        response = self.request.POST.get('recaptcha_response_field')
+        response  = self.request.POST.get('recaptcha_response_field')
         remote_ip = self.request.remote_addr
-
-        cResponse = captcha.submit(
-            challenge,
-            response,
-            self.app.config.get('captcha_private_key'),
-            remote_ip)
+        cResponse = captcha.submit  ( challenge
+                                    , response
+                                    , self.app.config.get('captcha_private_key')
+                                    , remote_ip)
 
         if not cResponse.is_valid:     
             self.flash('error', _('Wrong image verification code. Please try again.')
@@ -206,10 +194,9 @@ class DeleteAccountHandler(BaseHandler):
                         social.key.delete()
 
                     user_info.key.delete()
-
-                    ndb.Key("Unique", "User.username:%s" % user.username).delete_async()
+                    ndb.Key("Unique", "User.username:%s"    % user.username).delete_async()
                     ndb.Key("Unique", "User.auth_id:own:%s" % user.username).delete_async()
-                    ndb.Key("Unique", "User.email:%s" % user.email).delete_async()
+                    ndb.Key("Unique", "User.email:%s"       % user.email).delete_async()
 
                     #TODO: Delete UserToken objects
 
